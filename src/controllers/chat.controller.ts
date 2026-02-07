f255fa02c6b31970abf8498154b1689cb6271b86
Gonzalo Ebel <gonzalonicolasebel@gmail.com>
2026-02-07 15:33:47 -0300

    Colors added

--- src/controllers/chat.controller.ts
+++ src/controllers/chat.controller.ts
@@ -1,9 +1,28 @@
 import { Socket } from 'socket.io';
 
+
+const generateColor = (username: string) => {
+    let hash = 0;
+    for (let i = 0; i < username.length; i++) {
+        hash = username.charCodeAt(i) + ((hash << 5) - hash);
+    }
+    const red = (hash >> 0) & 255;
+    const green = (hash >> 8) & 255;
+    const blue = (hash >> 16) & 255;
+    const toHex = (num: number) => num.toString(16).padStart(2, '0');
+
+    console.log(`#${toHex(red)}${toHex(green)}${toHex(blue)}`)
+    return `#${toHex(red)}${toHex(green)}${toHex(blue)}`;
+}
+
 export const handleMessage = (socket: Socket, msg: any) => {
     socket.broadcast.emit('message', {
         body: msg.body,
-        from: msg.from,
+        from: {
+            id: socket.id,
+            username: msg.from.username, 
+            color: generateColor(msg.from.username)
+        },
         time: msg.time
     });
 };
