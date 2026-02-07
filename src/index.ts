a59c43a5aa52b03021496e62d3e7d76d5cc53b54
Gonzalo Ebel <gonzalonicolasebel@gmail.com>
2026-02-06 21:56:54 -0300

    Username

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,6 @@
 // Node.js essential modules
 import express from 'express';
 import http from 'http';
-import morgan from 'morgan' // HTTP request logger middleware
-import cors from 'cors' // CORS module
 import dotenv from 'dotenv' // Environment variable module
 import { Server as SocketServer } from 'socket.io' // Socket.IO for real-time communication
 // Configurations
@@ -34,16 +32,6 @@ io.on('connection', (socket) => {
     );
 })
 
-// Connect to the database
-
-// Set up middlewares
-// app.use(cors(corsOptions)); // Apply CORS middleware with options
-// app.use(morgan('dev')); // Use morgan for logging HTTP requests
-// app.use(express.json({ limit: '10mb' }));
-// app.use(express.urlencoded({ limit: '10mb', extended: true }));
-
-// Define routes
-// app.use('/api/ping', pingRoutes)
 
 // Start the server
 const port = process.env.PORT || 3000; // Server port from environment variables or default to 3000
