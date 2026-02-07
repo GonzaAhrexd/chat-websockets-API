be616c88c9fa0a96d30d0c48d9d534c69cde09be
Gonzalo Ebel <gonzalonicolasebel@gmail.com>
2026-02-07 00:29:34 -0300

    Date

--- src/controllers/chat.controller.ts
+++ src/controllers/chat.controller.ts
@@ -1,10 +1,10 @@
 import { Socket } from 'socket.io';
 
 export const handleMessage = (socket: Socket, msg: any) => {
-    console.log('Message received:', msg);
     socket.broadcast.emit('message', {
         body: msg.body,
-        from: msg.from
+        from: msg.from,
+        time: msg.time
     });
 };
 
@@ -13,7 +13,6 @@ export const handleDisconnect = (socket: Socket) => {
 };
 
 export const handleTyping = (socket: Socket, data: any) => {
-    console.log("Here")
     socket.broadcast.emit('typing', {
         user: data.user,
         isTyping: true
